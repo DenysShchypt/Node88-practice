bafe138ea026b325e234bdb96e282e7e50579b58
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2024-01-06 21:26:16 +0200

    change addProduct

--- controllers/productsControllers/getByIdProduct.js
+++ controllers/productsControllers/getByIdProduct.js
@@ -1,13 +1,13 @@
-import { getProductById } from "../../services/productsServices.js";
+import { Product } from "../models/productSchema.js";
 import { createError } from "../../helpers/index.js";
 
 const getByIdProduct = async (req, res) => {
-    const { id } = req.params;
-    const result = await getProductById(id);
-    if (!result) {
-        throw createError(404, `Product with ${id} not found`)
-    }
-    res.json(result)
+  const { id: _id } = req.params;
+  const result = await Product.findOneById(id);
+  if (!result) {
+    throw createError(404, `Product with ${id} not found`);
+  }
+  res.json(result);
 };
 
 export default getByIdProduct;
